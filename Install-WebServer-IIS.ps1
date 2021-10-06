929b410b8e292ac7d8d13c0730dcdfe0befb681a
maiaacademy <76057144+maiaacademy@users.noreply.github.com>
2021-10-05 22:22:10 -0300

    Update Install-WebServer-IIS.ps1

--- Install-WebServer-IIS.ps1
+++ Install-WebServer-IIS.ps1
@@ -2,4 +2,4 @@
 Add-WindowsFeature Web-Server
 
 # Set Default HTM
-Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Azure Expert is running $($env:computername)"
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Jornada Azure Expert is running $($env:computername)"
